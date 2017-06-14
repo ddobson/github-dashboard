1033d001775e9739dc5ba0ff9bd5f3db74bdcc12
ddobson <ddobson445@gmail.com>
2017-06-14 16:33:29 -0400

    Install ESLint - Add config file - Use Airbnb preset

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es6": true,
+        "browser": true,
+        "jest": true,
+    },
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ]
+};
